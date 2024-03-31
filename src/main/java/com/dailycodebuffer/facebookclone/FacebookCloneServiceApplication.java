27dadd6eeb90fbdbc943208c798df21c38d9b44b
Sayeed Anawar <49653193+sayeedanawar@users.noreply.github.com>
2024-03-31 22:45:03 +0530

    Merge pull request #3

--- src/main/java/com/dailycodebuffer/facebookclone/FacebookCloneServiceApplication.java
+++ src/main/java/com/dailycodebuffer/facebookclone/FacebookCloneServiceApplication.java
@@ -8,6 +8,7 @@ public class FacebookCloneServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FacebookCloneServiceApplication.class, args);
+        System.out.println("Hello");
     }
 
 }
